91854fe1b895cd453f4d8b23fa76f0a2389f3596
Hettiarachchi Gamage Mirosh kavinda <74175084+MK98-809@users.noreply.github.com>
2021-03-21 09:28:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import 'tachyons';
 import App from './containers/App'
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<App />,document.getElementById('react-root'));
 
 
