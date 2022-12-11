8d64eb3e19422ca295b2cc1c20b961145fd8f94c
mirosh-kavinda <kavindahgm@gmail.com>
2022-12-11 19:21:40 +0530

    initially adding local project to the remote repo

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,45 +1,50 @@
-import React,{Component} from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import './App.css';
-import Scroll from '../components/Scroll';
-
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import "./App.css";
+import Scroll from "../components/Scroll";
 
 class App extends Component {
-    constructor(){
-        super()
-        this.state={
-            robots:[],
-            searchfield:``
-        }
-    }
-componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users').then(response=>response.json())
-    .then(users=>this.setState({robots:users}));    
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: ``,
+    };
+  }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filterRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    });
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <div className="tc bg-light-green dib br5 pa3 ma2 grow tc2 shadow-5">
+          <img
+            alt="robots"
+            src={`https://robohash.org/${searchfield.toLowerCase()}?200x200`}
+          />
+        </div>
+        <Scroll>
+          <CardList robots={filterRobots} />
+        </Scroll>
+      </div>
+    );
+  }
 }
-
-onSearchChange = (event)=>{
-    this.setState({searchfield:event.target.value})
-}
-
-render(){
-    const{robots,searchfield}=this.state;
-    const filterRobots=robots.filter(robot=>{
-        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        return !robots.length?
-            <h1>Loading</h1>:
-            (
-                <div className='tc' >
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <CardList robots={filterRobots}/> 
-                    </Scroll>
-                </div>
-            );
-        }
-    }
-
 
 export default App;
